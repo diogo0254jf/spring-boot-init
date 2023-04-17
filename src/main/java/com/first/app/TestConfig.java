5ca2cd95464c6557f24956370a9ea70a02d6bdd8
maria menezes <diogo0254jf@gmail.com>
2023-04-17 11:57:17 -0300

    Category

--- src/main/java/com/first/app/TestConfig.java
+++ src/main/java/com/first/app/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.first.app.entities.Category;
 import com.first.app.entities.Order;
 import com.first.app.entities.User;
 import com.first.app.entities.enums.OrderStatus;
+import com.first.app.repositories.CategoryRepository;
 import com.first.app.repositories.OrderRepository;
 import com.first.app.repositories.UserRepository;
 
@@ -20,21 +22,30 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
         User u1 = new User(null, "Maria Brown", "maria@mail.com", "999999999", "123456");
         User u2 = new User(null, "Alex Green", "alex@mail.com", "888888888", "123456");
-        
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.DELIVERED, u1);
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 
---
+++ src/main/java/com/first/app/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.first.app.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.first.app.entities.Category;
+import com.first.app.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository CategoryRepository;
+
+    public List<Category> findAll() {
+        return CategoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = CategoryRepository.findById(id);
+        return obj.get();
+    }
+    
+}
