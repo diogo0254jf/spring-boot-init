969c72027b15686f816e60be6323589f4d07d2e2
maria menezes <diogo0254jf@gmail.com>
2023-04-17 15:53:19 -0300

    Payment

--- src/main/java/com/first/app/TestConfig.java
+++ src/main/java/com/first/app/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.first.app.entities.Category;
 import com.first.app.entities.Order;
 import com.first.app.entities.OrderItem;
+import com.first.app.entities.Payment;
 import com.first.app.entities.Product;
 import com.first.app.entities.User;
 import com.first.app.entities.enums.OrderStatus;
@@ -78,7 +79,9 @@ public class TestConfig implements CommandLineRunner {
         
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
-
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 
 }
