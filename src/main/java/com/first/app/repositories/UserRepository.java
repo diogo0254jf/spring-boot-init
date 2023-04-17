a8f3b6ff7625b4b07479d8261eb5a7bbc0d8f0d3
maria menezes <diogo0254jf@gmail.com>
2023-04-17 14:20:13 -0300

    OrderItem pt 1

--- src/main/java/com/first/app/repositories/UserRepository.java
+++ src/main/java/com/first/app/repositories/UserRepository.java
@@ -1,12 +1,9 @@
 package com.first.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.first.app.entities.User;
 
-
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
